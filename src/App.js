26be06034701bcc8d8bb4f6480495048ae36171b
itcuongthieu16 <it.thieutrancuong@gmail.com>
2023-01-16 10:17:27 +0700

    tdl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 function App() {
+  const [work, setWork] = useState("");
+  console.log(work);
   return (
-    <div className="App">
-      App
+    <div className="flex gap-8 h-screen justify-center items-center border border-red-500">
+      <input
+        type="text"
+        className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+        value={work}
+        onChange={(e) => setWork(e.target.value)}
+      />
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Add
+      </button>
     </div>
   );
 }
